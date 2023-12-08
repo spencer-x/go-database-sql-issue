36da2e18c922731653fe03136e1173cb466902b1
Shipeng Xie <shipengxie@outlook.com>
2023-12-07 17:33:29 -0800

    add code to reproduce issue

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/stdlib"
+	"golang.org/x/sync/errgroup"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	maxOpenConnections = 200
+	maxIdleConnections = 100
+	numWorkers         = 300
+	timeout            = 3 * time.Second
+	queryInterval      = 1 * time.Second
+)
+
+func main() {
+	cfg, err := pgx.ParseConfig("host=localhost port=5432 user=devbox password=secret dbname=postgres connect_timeout=30")
+	if err != nil {
+		panic(err)
+	}
+	db := stdlib.OpenDB(*cfg)
+	db.SetMaxIdleConns(maxIdleConnections)
+	db.SetMaxOpenConns(maxOpenConnections)
+	ctx := context.Background()
+	g, ctx := errgroup.WithContext(ctx)
+	var reqCount, errCount atomic.Int64
+	g.Go(func() error {
+		t := time.NewTicker(1000 * time.Millisecond)
+		start := time.Now()
+
+		for {
+			select {
+			case <-t.C:
+				fmt.Printf("time: %vs err/req (%d/%d) DB stats: %+v\n", time.Since(start).Seconds(), errCount.Swap(0), reqCount.Swap(0), db.Stats())
+			case <-ctx.Done():
+				t.Stop()
+				return ctx.Err()
+			}
+		}
+	})
+	for i := 0; i < numWorkers; i++ {
+		g.Go(func() error {
+			t := time.NewTicker(queryInterval)
+			for {
+				select {
+				case <-ctx.Done():
+					t.Stop()
+					return ctx.Err()
+				case <-t.C:
+					ctx, cancel := context.WithTimeout(ctx, timeout)
+					defer cancel()
+					if err := connectWithDB(ctx, db); err != nil {
+						errCount.Add(1)
+					}
+					reqCount.Add(1)
+				}
+			}
+		})
+		time.Sleep(queryInterval / numWorkers)
+	}
+	g.Wait()
+}
+
+func connectWithDB(ctx context.Context, db *sql.DB) error {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	var t time.Time
+	if err := tx.QueryRowContext(ctx, `select current_timestamp`).Scan(&t); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+	return nil
+}
